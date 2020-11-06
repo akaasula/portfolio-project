dc91aa3704f8e5bcf69157cb3740f2a9354c31c0
akaasula <akaasula@cisco.com>
2020-11-06 21:19:37 +0530

    modified blog/urls.p removed impirt include

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 import blog.views
-import include
+
 urlpatterns = [
     path('',blog.views.allblogs,name='allblogs'),
     path('<int:blog_id>/',blog.views.detail, name='detail')
